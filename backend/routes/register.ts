35bee5113e78f20c2c91b735e70aff5b651afe84
Kenny <kennysoyoye07@gmail.com>
2022-10-27 11:14:00 +0100

    created register route

---
+++ backend/routes/register.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import handleNewUser from "../controllers/registerController";
+
+const registerRouter = express.Router();
+
+registerRouter.post("/", handleNewUser);
+
+export default registerRouter;
