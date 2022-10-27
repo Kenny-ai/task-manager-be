372bb5f265a142e2b4fd365200d48417a7233746
Kenny <kennysoyoye07@gmail.com>
2022-10-27 11:09:34 +0100

    added login controller

---
+++ backend/controllers/authController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import User from "../model/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const handleLogin = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
+
+  let foundUser = await User.findOne({ username }).exec();
+
+  if (!foundUser)
+    return res.status(401).json({ message: `incorrect username or password` });
+
+  // evaluate password
+  const match = await bcrypt.compare(password, foundUser.password);
+
+  if (!match)
+    return res.status(401).json({ message: `incorrect username or password` });
+
+  const accessToken = jwt.sign(
+    { username },
+    `${process.env.ACCESS_TOKEN_SECRET}`,
+    {
+      expiresIn: "30m",
+    }
+  );
+  const refreshToken = jwt.sign(
+    { username },
+    `${process.env.REFRESH_TOKEN_SECRET}`,
+    {
+      expiresIn: "1d",
+    }
+  );
+
+  foundUser.refreshToken = refreshToken;
+  foundUser.save();
+
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+  res.status(200).json({ accessToken });
+};
+export default handleLogin;
