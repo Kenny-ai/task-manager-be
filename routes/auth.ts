cde560b570dd8a9ecc6d4ba623442057552ece3b
Kenny-ai <kennysoyoye07@gmail.com>
2023-08-30 10:50:27 +0100

    refactored auth routes

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,18 @@
-import express from "express";
-import handleLogin from "../controllers/Auth";
+import {
+  getAllUsers,
+  login,
+  logout,
+  refresh,
+  register,
+} from "../controllers/auth";
 
+const express = require("express");
 const authRouter = express.Router();
 
-authRouter.post("/", handleLogin);
+authRouter.post("/login", login);
+authRouter.get("/logout", logout);
+authRouter.get("/refresh", refresh);
+authRouter.post("/register", register);
+authRouter.get("/all_users", getAllUsers);
 
 export default authRouter;
