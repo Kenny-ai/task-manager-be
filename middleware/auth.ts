ea0b97ed731d8a439464804216646f09f5c9f368
Kenny-ai <kennysoyoye07@gmail.com>
2023-08-30 10:53:53 +0100

    added middleware to protect board routes

---
+++ middleware/auth.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface TokenBearer {
+  email: string;
+}
+
+export let tokenBearer: TokenBearer;
+
+export const protect = async (req: Request, res: Response, next: NextFunction) => {
+  let authHeader = req.headers.authorization as string;
+
+  if (!authHeader) return res.status(401).json({ message: "Invalid token" });
+
+  if (authHeader.toLowerCase().startsWith("bearer")) {
+    authHeader = authHeader.slice("bearer".length).trim();
+  }
+  jwt.verify(
+    authHeader,
+    `${process.env.ACCESS_TOKEN_SECRET}`,
+    (err: any, decoded: any) => {
+      if (err) return res.status(403).json({ message: "Forbidden" });
+      tokenBearer = decoded;
+      next();
+    }
+  );
+};
