878651b7c53874c97c651839a1f304efd3c05aaa
Kenny <kennysoyoye07@gmail.com>
2023-09-07 22:36:44 +0100

    Fixed cookies response bug
    changed the sameSite value from "none" to "Lax" to fix cookies response issue in development

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -39,7 +39,7 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "Lax",
       secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
