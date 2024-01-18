fe367a9c70623006b7beb45a567cba4facf93b40
Kenny-ai <kennysoyoye07@gmail.com>
2024-01-19 00:39:25 +0100

    changed api route to api/v1

--- server.ts
+++ server.ts
@@ -24,8 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/auth", authRouter);
-app.use("/boards", boardRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/boards", boardRouter);
 
 const message = {
   name: "Kenny",
